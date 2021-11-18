3e54aa89a083317eeed6a6ca99b20160a3bca5b6
RachelSparto <rmbachman2016@hotmail.com>
2021-11-18 16:13:04 -0500

    first commit

---
+++ public/src/accounts.js
@@ -0,0 +1,16 @@
+function findAccountById(accounts, id) {
+  console.log("hello");
+}
+
+function sortAccountsByLastName(accounts) {}
+
+function getTotalNumberOfBorrows(account, books) {}
+
+function getBooksPossessedByAccount(account, books, authors) {}
+
+module.exports = {
+  findAccountById,
+  sortAccountsByLastName,
+  getTotalNumberOfBorrows,
+  getBooksPossessedByAccount,
+};
